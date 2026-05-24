6c95f7ef4a5935573d5733be074acde1639548a4
tharukamaduwantha <tharukamaduwantha62@gmail.com>
2026-05-24 16:30:42 +0530

    docs: update student query filtering parameter specifications

--- backend/controllers/student_controller.h
+++ backend/controllers/student_controller.h
@@ -10,10 +10,7 @@
 
 #include "../include/civetweb.h"
 
-/*
- * Handles all /api/students and /api/students/{id} requests.
- * Supported methods: GET, POST, PUT, DELETE
- */
+/* Handles all /api/students and /api/students/{id} requests (GET, POST, PUT, DELETE) */
 int UsersHandler(struct mg_connection *conn, void *cbdata);
 
 #endif /* STUDENT_CONTROLLER_H */
