2660b039a4d1186adaa5fa3426e4351c7789e68d
tharukamaduwantha <tharukamaduwantha62@gmail.com>
2026-05-24 16:32:56 +0530

    docs: document JSON payload serialization boundaries and escape rules

--- backend/controllers/response_helper.c
+++ backend/controllers/response_helper.c
@@ -11,7 +11,7 @@
 #include <stdio.h>
 #include <time.h>
 
-/* Shared request counter – defined (and incremented) here */
+/* Shared request counter, incremented with each JSON response sent */
 unsigned requestCounter = 0;
 
 int SendJSONResponse(struct mg_connection *conn,
